52d73bb0efec280cb0d7817fa3b189c13366f899
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-06-09 11:10:06 -1000

    refactor: simplify playground

--- playground/src/pages/_app.tsx
+++ playground/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 
-import "@/assets/style.css";
+import "@/styles.css";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
 	return <Component {...pageProps} />;
